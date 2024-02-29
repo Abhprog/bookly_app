15a1c09d32f02fe57d73a3f52a57e5175f74687a
Mona Lisa <abh.prog@gmail.com>
2024-02-29 10:36:09 +0300

    4- refactor initstate & navigate to home

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,23 +1,90 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import '../../../../../core/utils/assets.dart';
+import '../../../../home/presentation/views/home_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+} // SplashViewBody
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    // each of the following two methods have their own roles,
+    // (without writing code dirctly in intiState() function),
+    // this is so called [Single Responsibility Principle]
+    initSlidingAnimation();
+    navigateToHomePage();
+  } // initState()
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  } // dispose()
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  } // initSlidingAnimation()
+
+  void navigateToHomePage() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+              pageBuilder: (_, __, ___) => const HomeView(),
+              transitionDuration: const Duration(milliseconds: 500),
+              transitionsBuilder: (_, a, __, c) {
+                const begin = Offset(0.0, 1.0);
+                const end = Offset.zero;
+                final tween = Tween(begin: begin, end: end);
+                final offsetAnimation = a.drive(tween);
+                return SlideTransition(
+                  position: offsetAnimation,
+                  child: c,
+                );
+              }));
+    });
+  } // navigateToHomePage()
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
           Image.asset(AssetsData.logo),
           const SizedBox(height: 10),
-          const Text('Read Free Books', textAlign: TextAlign.center),
-        ],
-      ),
-    );
+          SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                'Read Free Books',
+                textAlign: TextAlign.center,
+              ))
+        ]));
+
+    // TweenAnimationBuilder(
+    //       builder: (BuildContext context, Object? value, Widget? child) {
+    //     return SlideTransition(
+    //         position: slidingAnimation,
+    //         child:
+    //             const Text('Read Frnullee Books', textAlign: TextAlign.center));
+    //   }, tween: null, duration: , null)
   } // build()
 } // SplashViewBody
